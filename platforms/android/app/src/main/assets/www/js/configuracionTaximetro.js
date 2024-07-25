6c4bdb924bdfc2725439a3499d59c8b8dbcf8232
IngBrayanLeon= <sistemas.tecmmas2@gmail.com=>
2024-07-25 09:48:29 -0500

    inicio de repositorio

--- platforms/android/app/src/main/assets/www/js/configuracionTaximetro.js
+++ platforms/android/app/src/main/assets/www/js/configuracionTaximetro.js
@@ -1,10 +1,21 @@
+var taximetro = new Object();
 var url = localStorage.getItem('url');
+
 document.addEventListener('deviceready', function () {
     if (localStorage.getItem('ip') !== undefined && localStorage.getItem('ip') !== '') {
         $("#ip").val(localStorage.getItem('ip'));
     }
+    if (localStorage.getItem('idconfiguracion') !== undefined && localStorage.getItem('idconfiguracion') !== null && localStorage.getItem('idconfiguracion') !== '') {
+        getCiudades(localStorage.getItem('idconfiguracion'));
+    }
+    getCiudades(0);
+
+});
+
+function getCiudades(idconfiguracion) {
     var datos = {
-        function: "getCiudades"
+        function: "getCiudades",
+        idconfiguracion: idconfiguracion
     }
     fetch(url + "Ctaximetro",
         {
@@ -17,14 +28,17 @@ document.addEventListener('deviceready', function () {
         })
         .then(respuesta => respuesta.json())
         .then((rta) => {
-            localStorage.setItem('configuraciones', JSON.stringify(rta));
-            $.each(rta, function (i, data) {
+            
+            cargarDatos(rta.idconfiguracion);
+            
+            localStorage.setItem('configuraciones', JSON.stringify(rta.ciudades));
+            $.each(rta.ciudades, function (i, data) {
                 $('#selectConfigTaximetro').append("<option  value=" + data.idconfiguracion + ">" + data.ciudad + "</option>");
             });
         })
         .catch(error => {
             console.log(error.message);
-            MySwal.fire({
+            Swal.fire({
                 position: 'center',
                 icon: 'error',
                 text: 'Error en servidor ' + error.message,
@@ -33,7 +47,7 @@ document.addEventListener('deviceready', function () {
                 // timer: 1500
             })
         })
-});
+}
 
 
 function selectCiudad() {
@@ -41,7 +55,7 @@ function selectCiudad() {
     var configuracions = JSON.parse(localStorage.getItem('configuraciones'));
     var idconfiguracion = JSON.parse(localStorage.getItem('idconfiguracion'));
     var key = Object.keys(configuracions[0]);
-    $.each(configuracions, function (i, data) {        
+    $.each(configuracions, function (i, data) {
         if (idconfiguracion == data.idconfiguracion) {
             document.getElementById('banderazoPesos').value = data.banderazoPesos;
             document.getElementById('banderazoUnidad').value = data.banderazoUnidad;
@@ -54,18 +68,7 @@ function selectCiudad() {
             document.getElementById('pesosUnidad').value = data.pesosUnidad;
             document.getElementById('tiempoCaida').value = data.tiempoCaida;
             document.getElementById('unidad').value = data.unidad;
-            // var data2 = data;
-            // for (var j = 0; j < key.length; j++) {
-            //     if (document.getElementById(key[j])) {
-            //         console.log(document.getElementById(key[j]));    
-            //         console.log(key[j]);
-            //         console.log(typeof(key[j]) );
-            //         console.log(document.getElementById(key[j]).value = data.key[j]);
-            //         document.getElementById(key[j]).value = dat2[j];
-            //         break;
-            //     }
-
-            // }
+            setearDatosTaximetro();
         }
 
     });
@@ -73,6 +76,56 @@ function selectCiudad() {
 
 }
 
+function cargarDatos(dat){
+    
+    console.log(dat);
+    // document.getElementById('banderazoPesos').value = data.banderazoPesos;
+    //         document.getElementById('banderazoUnidad').value = data.banderazoUnidad;
+    //         document.getElementById('distanciaCaida').value = data.distanciaCaida;
+    //         document.getElementById('divistorDistancia').value = data.divistorDistancia;
+    //         document.getElementById('divistorVelocidad').value = data.divistorVelocidad;
+    //         document.getElementById('numCaidasDistancia').value = data.numCaidasDistancia;
+    //         document.getElementById('numCaidasTiempo').value = data.numCaidasTiempo;
+    //         document.getElementById('perimetro').value = data.perimetro;
+    //         document.getElementById('pesosUnidad').value = data.pesosUnidad;
+    //         document.getElementById('tiempoCaida').value = data.tiempoCaida;
+    //         document.getElementById('unidad').value = data.unidad;
+}
+
+function setearDatosTaximetro() {
+    taximetro.tiempo = 0.0;
+    taximetro.distancia = 0.0;
+}
+
 function guradarIp() {
     localStorage.setItem('ip', $("#ip").val());
 }
+
+
+var cronometro;
+var inicio = false;
+
+var tIniciar = function () {
+    if (!inicio) {
+        inicio = true;
+        cronometro = setInterval(function () {
+            taximetro.tiempo = taximetro.tiempo + 100;
+            if ($("#tiempo") !== undefined) {
+                var tiempo = taximetro.tiempo / 1000;
+                $("#tiempo").text(tiempo.toFixed(1));
+            }
+        }, 100);
+    }
+};
+var tPausar = function () {
+    inicio = false;
+    clearInterval(cronometro);
+};
+var tReset = function () {
+    inicio = false;
+    taximetro.tiempo = 0.0;
+    taximetro.distancia = 0.0;
+    if ($("#tiempo") !== undefined)
+        $("#tiempo").text(taximetro.tiempo);
+    clearInterval(cronometro);
+};
--- platforms/android/app/src/main/assets/www/js/modbus.js
+++ platforms/android/app/src/main/assets/www/js/modbus.js
@@ -1,35 +1,48 @@
-
+var conectado = false;
 document.addEventListener('deviceready', function () {
-    conectarDHT('192.168.1.38', '502');
+    var ip = localStorage.getItem('ip');
+    if (ip == undefined || ip == null || ip == "") {
+        $("#conec").html("<label style='color: green'>Asigne la ip</label>");
+    } else {
+        conectarDHT(ip, '502');
+    }
 
 
 });
 
-
+function validaConect() {
+    if (!conectado) {
+        conectarDHT($("#ip").val(), '502');
+    }
+}
 
 
 function conectarDHT(ip, puerto) {
     cordova.exec(function (data) {
-        console.log(data)
-        escribirDHT(1, 155);
-        setInterval(function () { leerDHT() }, 1000);
+        $("#conec").html("<label style='color: green'>Conectado</label>");
+        conectado = true;
+        setInterval(function () {
+            leerDHT()
+        }, 1000);
 
     }, function (error) {
-
         console.log(error);
+        $("#conec").html("<label style='color: red'>Desconectado.</label>");
     }, "mds", "open", [ip, puerto]);
 }
 
 function escribirDHT(R, D) {
     cordova.exec(function (data) {
         console.log(data);
+        conectado = true;
     }, function (error) {
-        var datos = JSON.parse(localStorage.getItem('datos'))
-        conectarDHT(datos.ip, datos.puerto);
+
         //        $("#alert").show();
         ////        $("#mesaje").html('Intentelo nuevamente.' + error);
         //        $("#mesaje").html('Intentelo nuevamente.');
         console.log(error);
+        conectado = false;
+        $("#conec").html("<label style='color: red'>Desconectado.</label>");
     }, "mds", "escribir", [R, D]);
 }
 
@@ -38,9 +51,20 @@ function leerDHT() {
         var res = data.split("|")
         console.log(res[1])
         $("#pulsos").text(res[0]);
-        
+
         console.log(data);
+        conectado = true;
     }, function (error) {
         console.log(error);
+        conectado = false;
+        $("#conec").html("<label style='color: red'>Desconectado.</label>");
     }, "mds", "leer", [60]);
+}
+
+function desConectarDHT() {
+    cordova.exec(function (data) {
+        conectado = false;
+    }, function (error) {
+        console.log(error);
+    }, "mds", "desconectar", []);
 }
